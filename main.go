0e3e67210428afe6f1af3dd30746af2dedb03229
Alexander Thaller <alexander.thaller@trivago.com>
2016-02-09 20:44:15 +0100

    Added function that waits for typical unix stop signals.

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package httphelper
 
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 var (
 	Stopping bool
 )
+
+func WaitForStopSignal() {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
+	<-signals
+}
