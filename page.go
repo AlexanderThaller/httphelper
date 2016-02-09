50a3dc82e4e9766fa15847006d68947770528580
Alexander Thaller <alexander.thaller@trivago.com>
2016-02-09 20:25:46 +0100

    Added minimal favicon.

--- page.go
+++ page.go
@@ -7,18 +7,28 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func pageRouterNotFound(w http.ResponseWriter, r *http.Request, p httprouter.Params) *HandlerError {
+func PageRouterNotFound(w http.ResponseWriter, r *http.Request, p httprouter.Params) *HandlerError {
 	return NewHandlerError(errgo.New("page not found"), http.StatusNotFound)
 }
 
-func pageRouterMethodNotAllowed(w http.ResponseWriter, r *http.Request, p httprouter.Params) *HandlerError {
+func PageRouterMethodNotAllowed(w http.ResponseWriter, r *http.Request, p httprouter.Params) *HandlerError {
 	return NewHandlerError(errgo.New("method not allowed"), http.StatusMethodNotAllowed)
 }
 
-func pageRouterStopping(w http.ResponseWriter, r *http.Request, p httprouter.Params) *HandlerError {
+func PageRouterStopping(w http.ResponseWriter, r *http.Request, p httprouter.Params) *HandlerError {
 	return NewHandlerError(errgo.New("service is stopping"), http.StatusInternalServerError)
 }
 
-func pageFiles(w http.ResponseWriter, r *http.Request, p httprouter.Params) *HandlerError {
-	return NewHandlerError(errgo.New("not implemented"), http.StatusInternalServerError)
+func PageMinimalFavicon(w http.ResponseWriter, r *http.Request, p httprouter.Params) *HandlerError {
+	raw, err := Asset("data/favicon.ico")
+	if err != nil {
+		return NewHandlerError(errgo.Notef(err, "can not read raw page"), http.StatusInternalServerError)
+	}
+
+	_, err = w.Write(raw)
+	if err != nil {
+		return NewHandlerError(errgo.Notef(err, "can not write raw data to responsewriter"), http.StatusInternalServerError)
+	}
+
+	return nil
 }
